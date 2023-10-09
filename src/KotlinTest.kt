213fb84ef959e8660f211b4284118d5ef1208d1e
Kacper Marczewski <kacper.marczewski@gmail.com>
2023-10-09 17:12:58 +0200

    primes

--- src/KotlinTest.kt
+++ src/KotlinTest.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
-    println("test");
+    val limit = 100 // Change this to your desired limit
+    val primes = mutableListOf<Int>()
+    val isPrime = BooleanArray(limit + 1) { true }
+
+    for (p in 2..limit) {
+        if (isPrime[p]) {
+            primes.add(p)
+            for (i in p * p..limit step p) {
+                isPrime[i] = false
+            }
+        }
+    }
+
+    println("Prime numbers up to $limit are:")
+    println(primes)
 }
