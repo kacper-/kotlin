83ba5e307b691d4a9f0a36edf698010396827656
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-08-06 23:46:14 +0200

    java kotlin and equals test

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,12 +1,8 @@
 class HelloWorld {
     fun run() {
-        val person = Person("John Doe", 30)
+        val p1 = Person("John Doe", 30)
+        val p2 = Person("John Doe", 30)
 
-        val info = with(person) {
-            println("Name: $name, Age: $age")
-            "Name: $name, Age: $age"
-        }
-
-        println("Person Info: $info")
+        println("equals kotlin " + (p1 == p2))
     }
 }
---
+++ src/JClass.java
@@ -0,0 +1,5 @@
+public class JClass {
+    void print(Object o1, Object o2) {
+        System.out.println("equals java " + (o1 == o2));
+    }
+}
